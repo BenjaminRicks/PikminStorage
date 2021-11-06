3f6a0b947b1fa303246c790808c6fa059a01fb4b
Benjamin Ricks <benjamin_ricks@att.net>
2021-11-06 11:38:33 -0600

    Created vue stuff and added data

---
+++ pikmin/src/views/PikminStorage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pikminStorage">
+    <pikmin msg="Pikmin Storage"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+  name: 'PikminStorage',
+  components: {
+  }
+}
+</script>
+
